958a563890d95a6d27f08356bf6f11a746e78c0a
ryuh2929 <ryuh2929@gmail.com>
2026-04-03 17:10:44 +0900

    init: change environment to uv

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Pass-Master-GraphRAG
+### uv 사용자
+uv sync
+
+### pip 사용자
+pip install -r requirements.txt
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pass-master-graphrag!")
+
+
+if __name__ == "__main__":
+    main()
